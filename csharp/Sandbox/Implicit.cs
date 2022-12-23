f4b65e64b48a19be5f0b751eb2cfaba04480a6b2
Cole Shepherd <cole@recroom.com>
2022-12-23 12:16:09 -0800

    Removed old stuff, now experimenting with implicit parameters.

---
+++ csharp/Sandbox/Implicit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+
+namespace Sandbox;
+
+public struct Implicit<T>
+{
+    public class WithScope : IDisposable
+    {
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _valueStack.Value = _valueStack.Value!.Pop();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public static WithScope With(T t)
+    {
+        _valueStack.Value = _valueStack.Value!.Push(t);
+        return new WithScope();
+    }
+
+    private static AsyncLocal<ImmutableStack<T>> _valueStack;
+
+    static Implicit()
+    {
+        _valueStack = new();
+        _valueStack.Value = ImmutableStack<T>.Empty;
+    }
+
+    public T Value => _valueStack.Value!.Peek();
+}
--- csharp/Sandbox/Program.cs
+++ csharp/Sandbox/Program.cs
@@ -1,84 +1,32 @@
-﻿namespace Sandbox;
+﻿// Ideas pure/impure analyzer
+// Warn concrete uses of non-DI'd stuff
+
+namespace Sandbox;
+
+public interface IConsole
+{
+    void WriteLine(string? value);
+}
+
+public class ConsoleImpl : IConsole
+{
+    public void WriteLine(string? value) => Console.WriteLine(value);
+}
 
 public static class Cli
 {
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        //await GenerateIdris2Bindings(
-        //    @"C:\Users\R_SD\dev\personal\sandbox\idris2\examples\ffilib.c",
-        //    "ffilib",
-        //    @"C:\Users\R_SD\dev\personal\sandbox\idris2\examples",
-        //    new()
-        //    {
-        //    },
-        //    new()
-        //    {
-        //        "deref_as_int",
-        //        "cast_to_anyptr"
-        //    },
-        //    new()
-        //    {
-        //        "System.FFI"
-        //    },
-        //    new()
-        //    {
-        //    });
+        ConsoleImpl consoleImpl = new();
+        
+        using var _ = Implicit<IConsole>.With(consoleImpl);
+        using var _2 = Implicit<IConsole>.With(consoleImpl);
 
-        await GenerateIdris2Bindings(
-            "C:/Users/R_SD/dev/personal/SDL/include/SDL.h",
-            "SDL2",
-            @"C:\Users\R_SD\dev\personal\sandbox\idris2\examples\pong",
-            new()
-            {
-                "SDL_Event",
-                "SDL_Rect"
-            },
-            new()
-            {
-                "SDL_Init",
-                "SDL_CreateWindow",
-                "SDL_GetWindowSurface",
-                "SDL_UpdateWindowSurface",
-                "SDL_Delay",
-                "SDL_CreateRenderer",
-                "SDL_RenderSetLogicalSize",
-                "SDL_RenderClear",
-                "SDL_RenderPresent",
-                "SDL_RenderDrawRect",
-                "SDL_RenderFillRect",
-                "SDL_SetRenderDrawColor",
-                "SDL_PollEvent"
-            },
-            new()
-            {
-                "Core",
-                "System.FFI",
-                "Ffilib"
-            },
-            new()
-            {
-                ("SDL_QUIT", "Int", "0x100"),
-                ("SDL_INIT_VIDEO", "Int", "0x00000020"),
-                ("SDL_RENDERER_ACCELERATED", "Int", "0x00000002"),
-                ("SDL_KEYDOWN", "Int", "0x300"),
-                ("SDL_KEYUP", "Int", "0x301"),
-                ("SDLK_w", "Int", "77"),
-                ("SDLK_s", "Int", "73"),
-            });
+        RunHelloWorld();
     }
 
-    public static async Task GenerateIdris2Bindings(
-        string cSourceFilePath,
-        string moduleName,
-        string outDirectoryPath,
-        HashSet<string> typeNameWhitelist,
-        HashSet<string> functionNameWhitelist,
-        HashSet<string> imports,
-        List<(string, string, string)> constants)
+    public static void RunHelloWorld(Implicit<IConsole> console = default)
     {
-        Program program = CParser.ParseProgram(cSourceFilePath, typeNameWhitelist, functionNameWhitelist);
-
-        string idrisSourceCode = IdrisCodeGenerator.GenerateIdrisSourceCode(moduleName, program, imports, constants);
-        await File.WriteAllTextAsync(Path.Combine(outDirectoryPath, $"{moduleName}.idr"), idrisSourceCode);
+        console.Value.WriteLine("Hello, world!");
     }
 }
