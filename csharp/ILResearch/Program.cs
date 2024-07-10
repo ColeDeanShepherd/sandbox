14b8c9d71398bbe39acbabf78a3f4834c99bea98
Cole Shepherd <cole@coledeanshepherd.com>
2024-07-10 11:22:18 -0700

    Progress

---
+++ csharp/ILResearch/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    static void Main(/*string[] args*/)
+    {
+        //Console.WriteLine("Hello, World!");
+        var x = new[] { 1, 2, 3 };
+        //var y = x.Select(a => 2 * a).ToArray();
+    }
+}
---
+++ csharp/OneBillionRowChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OneBillionRowChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
